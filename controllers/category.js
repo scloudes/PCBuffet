93e95a1eded9a60fa34562c3aaa1d0abe9d5f0a4
scloudes <sebascloud2001@gmail.com>
2021-07-09 14:27:09 -0500

    Category model edited

--- controllers/category.js
+++ controllers/category.js
@@ -11,8 +11,8 @@ const getCategories = async (req, res, next) => {
 
 const createCategory = async (req, res, next) => {
   try {
-    const { category, questions } = req.body;
-    const newCategory = new Category({ category, questions });
+    const { title, category, questions } = req.body;
+    const newCategory = new Category({ title, category, questions });
     const categorySaved = await newCategory.save();
     res.status(201).json(categorySaved);
   } catch (err) {
@@ -35,7 +35,9 @@ const updateCategory = async (req, res, next) => {
 
 const deleteCategory = async (req, res, next) => {
   try {
-    const deletedCategory = await Category.findByIdAndDelete(req.params.categoryId);
+    const deletedCategory = await Category.findByIdAndDelete(
+      req.params.categoryId
+    );
     res.status(200).json(deletedCategory);
   } catch (err) {
     next(err);
--- models/category.js
+++ models/category.js
@@ -2,6 +2,7 @@ const { Schema, model } = require("mongoose");
 
 const categorySchema = new Schema(
   {
+    title: String,
     category: String,
     questions: Object,
   },
